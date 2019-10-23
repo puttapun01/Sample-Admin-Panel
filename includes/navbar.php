41ec51a233de105936cc2350e4ba11b4c2c18673
puttapun01 <puttapun01@gmail.com>
2019-10-24 00:59:19 +0700

    Finish Part 9

--- includes/navbar.php
+++ includes/navbar.php
@@ -339,11 +339,8 @@
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
 
           <form action="logout.php" method="POST">
-
             <button type="submit" name="logout_btn" class="btn btn-primary">Logout</button>
-
           </form>
-
 
         </div>
       </div>
